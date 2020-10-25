be2bfedd65766c33d02a02b6f92e942380b2f312
Pieter Herman <identity@pieterherman.dev>
2020-10-25 23:42:22 +0100

    Created questions.json , created frist setup for Quiz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Alert from '@material-ui/core/Button';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  var questions = require('./data/questions.json');
+
+ 
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  let [showAlert, setAlert] = useState(false);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+      setAlert(showAlert = true);
+      setScore(score + 1);
+      
+		} else {
+      setAlert(showAlert = false);
+
+    }
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+  
+
+ 
+		<div className='app'>
+ 
+
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span> {currentQuestion + 1}</span>/{questions.length}
+          	</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+   
+				</>
+			)}
+		</div>
+	);
+}
